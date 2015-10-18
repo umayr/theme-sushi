0800790ea075b3c0ea2d5ccb495102be01120982
Umayr Shahid <umayrr@hotmail.co.uk>
2015-10-18 21:33:25 +0500

    (feat) Move colors to greetings file

---
+++ fish_greeting.fish
@@ -0,0 +1,12 @@
+set -g CMD_DURATION 0
+
+function yellow;	    set_color -o b58900; end
+function red;       	set_color -o d30102; end
+function cyan;      	set_color -o 2aa198; end
+function white;			set_color -o fdf6e3; end
+function dim;			set_color -o 333333; end
+function off;			set_color -o normal; end
+
+function fish_greeting
+	echo (dim)(uname -mnprs)(off)
+end
