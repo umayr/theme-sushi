c7d0bc9acd08df27140a1e9a75773713f2e94e7b
Umayr Shahid <umayrr@hotmail.co.uk>
2015-10-18 21:08:57 +0500

    (feat) Add braces around cwd

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,25 +1,27 @@
-function ylw; set_color -o FFAA00; end
-function red; set_color -o CC0000; end
-function trd; set_color -o 666666; end
-function dim; set_color -o 333333; end
-function off; set_color normal; end
+set -g CMD_DURATION 0
 
+function yellow;	    set_color -o b58900; end
+function red;       	set_color -o d30102; end
+function cyan;      	set_color -o 2aa198; end
+function white;			set_color -o fdf6e3; end
+function dim;			set_color -o 333333; end
+function off;			set_color -o normal; end
 
 function fish_right_prompt
-  
-  if test "$theme_short_path" = 'yes'
-    set cwd (basename (prompt_pwd))
 
-    if git_is_repo
-      set root_folder (command git rev-parse --show-toplevel ^/dev/null)
-      set parent_root_folder (dirname $root_folder)
-      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-    end
+	if test "$theme_complete_path" = "yes"
+		set cwd (prompt_pwd)
+	else
+		set cwd (basename (prompt_pwd))
 
-  else
-    set cwd (prompt_pwd)
-  end
+		if git_is_repo
+			set root_folder (command git rev-parse --show-toplevel ^/dev/null)
+			set parent_root_folder (dirname $root_folder)
+			set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+		end
+	end
 
-  printf (ylw)"$cwd"(off)
+	printf (yellow)"("(dim)$cwd(yellow)") "(off)
+	printf (dim)(date +%H(yellow):(dim)%M(yellow):(dim)%S)(off)
 
 end
